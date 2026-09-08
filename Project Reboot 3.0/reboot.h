70210cfe991b30274282128d52c96587b7af9018
Bertogim <biosgim@gmail.com>
2026-09-08 16:16:55 +0200

    Fix GetAllObjectsOfClass: Cast before push_back (broken for T != UObject)

--- Project Reboot 3.0/reboot.h
+++ Project Reboot 3.0/reboot.h
@@ -245,7 +245,7 @@ inline std::vector<T*> GetAllObjectsOfClass(UClass* Class)
 			continue;
 
 		if (Object->IsA(Class))
-			Objects.push_back(Object);
+			Objects.push_back(Cast<T>(Object));
 	}
 
 	return Objects;
